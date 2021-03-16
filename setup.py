10d3daec52b6d41ddd42316cef5d9ccafbb3771c
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-03-16 17:45:11 +0100

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from requests import get
+
+setup(
+      name='vtools',
+      version=get("https://api.github.com/repos/CastellaniDavide/vtools/tags").json()[0]['name'].replace("v", "") if len(get("https://api.github.com/repos/CastellaniDavide/vtools/tags").json()) > 0 else "0.0", # Lastest release
+      description=get("https://api.github.com/repos/CastellaniDavide/vtools").json()['description'],
+      long_description=get("https://raw.githubusercontent.com/CastellaniDavide/vtools/master/docs/README.md").text,
+      long_description_content_type="text/markdown",
+      url=get("https://api.github.com/repos/CastellaniDavide/vtools").json()['html_url'],
+      author=get("https://api.github.com/repos/CastellaniDavide/vtools").json()['owner']['login'],
+      author_email=get(f"https://api.github.com/users/{get('https://api.github.com/repos/CastellaniDavide/vtools').json()['owner']['login']}").json()['email'],
+      license='GNU',
+      packages=find_packages(),
+      python_requires=">=3.7",
+      platforms="linux_distibution",
+      install_requires=[i for i in get("https://raw.githubusercontent.com/CastellaniDavide/vtools/master/requirements/requirements.txt").text.split("\n") if not "#" in i and i != ''],
+      zip_safe=True
+      )
