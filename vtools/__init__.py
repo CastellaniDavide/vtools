94181a3c0d4bb72ff7b146880549b55cffbc2c6a
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-03-16 17:45:12 +0100

    Created vtools/__init__.py

---
+++ vtools/__init__.py
@@ -0,0 +1,19 @@
+"""vtools
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-3-16"
+
+class vtools:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(vtools.vtools())
+	
+	def vtools():
+		"""vtools first funtion
+		"""
+		return "vtools" #for the test (see test_vtools.py)
+		
+if __name__ == "__main__":
+	vtools()
