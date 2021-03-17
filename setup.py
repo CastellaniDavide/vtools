5681474f02110187e4e09a929c7ab93fb0e41e52
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-03-17 15:51:47 +0100

    tabular-log

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
       packages = ['src'],
       python_requires=">=3.6",
       platforms="linux_distibution",
-      install_requires=["requests"],
+      install_requires=["requests", "tabular-log"],
       zip_safe=True,
       entry_points = {
         'console_scripts': [
