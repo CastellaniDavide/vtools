88f40960b720d352ee2af6cf98ac216d06a515aa
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-03-17 08:05:00 +0100

    #tabular-log

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup
 import sys,os
+from requests import get
 
 setup(
       name='vtools',
-      version="1.0",
-      description="vtools by Castellani Davide",
-      long_description="Manage vitual machines, getting some informations (eg. OS).",
+      version=get("https://api.github.com/repos/CastellaniDavide/vtools/tags").json()[0]['name'].replace("v", "") if len(get("https://api.github.com/repos/CastellaniDavide/vtools/tags").json()) > 0 else "0.0", # Lastest release
+      description=get("https://api.github.com/repos/CastellaniDavide/vtools").json()['description'],
+      long_description=get("https://raw.githubusercontent.com/CastellaniDavide/vtools/master/docs/README.md").text,
       long_description_content_type="text/markdown",
-      url="https://github.com/CastellaniDavide/vtools",
-      author="DavideC03",
-      author_email="help@castellanidavide.it",
+      url=get("https://api.github.com/repos/CastellaniDavide/vtools").json()['html_url'],
+      author=get("https://api.github.com/repos/CastellaniDavide/vtools").json()['owner']['login'],
+      author_email=get(f"https://api.github.com/users/{get('https://api.github.com/repos/CastellaniDavide/vtools').json()['owner']['login']}").json()['email'],
       license='GNU',
       packages = ['src'],
       python_requires=">=3.6",
       platforms="linux_distibution",
-      install_requires=["tabular-log"],
+      install_requires=[i for i in get("https://raw.githubusercontent.com/CastellaniDavide/pypi-template/master/requirements/requirements.txt").text.split("\n") if not "#" in i and i != ''],
       zip_safe=True,
       entry_points = {
         'console_scripts': [
-            'vtools=src.vtools:vtools']
+            'vtools=src.vtools:laucher']
             },
       )
