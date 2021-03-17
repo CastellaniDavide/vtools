a3d1f761ec12fb3cc71a42c69a8dcde96b1ca440
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-03-17 07:18:39 +0100

    Updated setup.py w/ real data

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import sys,os
 
 setup(
       name='vtools',
       version="1.0",
-      description="desc",
-      long_description="aaa",
+      description="vtools by Castellani Davide",
+      long_description="Manage vitual machines, getting some informations (eg. OS).",
       long_description_content_type="text/markdown",
-      url="a",#get("https://api.github.com/repos/CastellaniDavide/vtools").json()['html_url'],
-      author="aaa",#get("https://api.github.com/repos/CastellaniDavide/vtools").json()['owner']['login'],
-      author_email="aaa",#get(f"https://api.github.com/users/{get('https://api.github.com/repos/CastellaniDavide/vtools').json()['owner']['login']}").json()['email'],
+      url="https://github.com/CastellaniDavide/vtools",
+      author="DavideC03",
+      author_email="help@castellanidavide.it",
       license='GNU',
       packages = ['src'],
       python_requires=">=3.6",
       platforms="linux_distibution",
-      install_requires=[],#i for i in get("https://raw.githubusercontent.com/CastellaniDavide/vtools/master/requirements/requirements.txt").text.split("\n") if not "#" in i and i != ''],
+      install_requires=["tabular_log"],
       zip_safe=True,
       entry_points = {
         'console_scripts': [
