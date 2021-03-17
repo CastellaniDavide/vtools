faed52efa147226d98d7a34c4759f9ac1f334d4e
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-03-17 21:40:51 +0100

    Added publish-choco

--- src/vtools.py
+++ src/vtools.py
@@ -7,7 +7,7 @@ from csv import DictWriter
 from tabular_log import tabular_log
 
 __author__ = "help@castellanidavide.it"
-__version__ = "1.0 2021-3-16"
+__version__ = "01.01 2021-3-16"
 
 class vtools:
 	def __init__ (self, other=None):
@@ -28,7 +28,7 @@ class vtools:
 		# Define main variabiles
 		self.verbose = True
 		self.csv = True
-		self.vboxmanage = "C:/Program Files/Oracle/VirtualBox/vboxmanage" if "--choco" in argv or "--choco" == other else "vboxmanage"
+		self.vboxmanage = '"C:/Program Files/Oracle/VirtualBox/vboxmanage"' if "--choco" in argv or "--choco" == other else "\"C:/Program Files/Oracle/VirtualBox/VBoxManage.exe\""
 
 		# Define log
 		self.log = tabular_log(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "log", "trace.log") if "--choco" in argv or "--choco" == other else "trace.log", title = "vtools" ,verbose = self.verbose)
@@ -36,8 +36,8 @@ class vtools:
 
 		if self.csv:
 			# Define files
-			self.OS = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "flussi", "OS.csv") if "--choco" in argv or "--choco" == other else "OS.csv"
-			self.net = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "flussi", "net.csv") if "--choco" in argv or "--choco" == other else "net.csv"
+			self.OS = "OS.csv" #os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "flussi", "OS.csv") if "--choco" in argv or "--choco" == other else "OS.csv"
+			self.net = "net.csv" #os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "flussi", "net.csv") if "--choco" in argv or "--choco" == other else "net.csv"
 			self.OSheader = "PC_name,OS"
 			self.net_header = "PC_name,network_card_name,V4,MAC,Attachment"
 			self.log.print("Defined CSV files' infos")
@@ -60,14 +60,20 @@ class vtools:
 	def core(self):
 		"""Core of all project
 		"""
-		for PC in self.vmachines:
+		for PC, PCcode in zip(self.vmachines, self.vmachinescodes):
 			if self.csv:
-				DictWriter(open(self.OS, 'a+', newline=''), fieldnames=self.OSheader.split(","), restval='').writerow({"PC_name": PC, "OS": self.get_os(PC)})
-				for i in self.get_net(PC):
-					net = {"PC_name": PC}
-					for key, value in zip(self.net_header.split(",")[1:], i):
-						net[key] = value
-					DictWriter(open(self.net, 'a+', newline=''), fieldnames=self.net_header.split(","), restval='').writerow(net)
+				try:
+					DictWriter(open(self.OS, 'a+', newline=''), fieldnames=self.OSheader.split(","), restval='').writerow({"PC_name": PC, "OS": self.get_os(PCcode)})
+				except:
+					self.log.print(f"Error taking {PC} OS")
+				for i in self.get_net(PCcode):
+					try:
+						net = {"PC_name": PC}
+						for key, value in zip(self.net_header.split(",")[1:], i):
+							net[key] = value
+						DictWriter(open(self.net, 'a+', newline=''), fieldnames=self.net_header.split(","), restval='').writerow(net)
+					except:
+						self.log.print(f"Error taking {PC} network ifos")
 
 				self.log.print("Stored into csv file(s)")
 
@@ -75,11 +81,14 @@ class vtools:
 		"""Get virtual machines' name
 		"""
 		self.vmachines = []
+		self.vmachinescodes = []
 		temp=""
 		take=False
+		temp2=""
+		take2=False
 
 		for i in self.get_output(["list", "vms"]):
-			if i == "\"":
+			if i == '"':
 				if take == True:
 					self.vmachines.append(temp)
 					temp = ""
@@ -87,18 +96,33 @@ class vtools:
 			elif take:
 				temp += i
 
-		self.log.print("Get VM names")
+			if i == '{' or i == '}':
+				if take2 == True:
+					self.vmachinescodes.append(temp2)
+					temp2 = ""
+				take2 = not take2
+			elif take2:
+				temp2 += i
+
+		self.log.print(f"Get VM names {self.vmachines} {self.vmachinescodes}")
 
 	def get_output(self, array):
 		""" Gets the output by the shell
 		"""
-		return str(subprocess.Popen([self.vboxmanage] + array, stdout=subprocess.PIPE).communicate()[0])
+		cmd = self.vboxmanage
+		for i in array:
+			if " " in i:
+				i = "'" + i + "'"
+			cmd += " "  + i
+
+		return vtools.remove_b(subprocess.check_output(cmd, shell=False))
 
 	def get_os(self, machine_name):
 		""" Gets the vitual machine os
 		"""
-		self.log.print("Getting OS")
-		return vtools.remove_b(self.get_output(["guestproperty", "get", machine_name, "/VirtualBox/GuestInfo/OS/Product"]).replace("Value: ", "").replace("\\n", "").replace("\\r", ""))
+		_os = self.get_output(["guestproperty", "get", machine_name, "/VirtualBox/GuestInfo/OS/Product"]).replace("Value: ", "").replace("\\n", "").replace("\\r", "")
+		self.log.print(f"Getted OS {_os}")
+		return _os
 
 	def remove_b(string):
 		"""Removes b'' by string
@@ -115,7 +139,7 @@ class vtools:
 		try:
 			propriety = "Count"
 			
-			for i in range(int(str(self.get_output(["guestproperty", "get", machine_name, f"/VirtualBox/GuestInfo/Net/{propriety}"]).replace("Value: ", "").replace("\\n", "").replace("\\r", "").replace("b'", "")[:-1:]))):
+			for i in range(int(self.get_output(["guestproperty", "get", machine_name, f"/VirtualBox/GuestInfo/Net/{propriety}"]).replace("Value: ", "").replace("\\n", "").replace("\\r", ""))):
 				for propriety in ["Name", "V4/IP", "MAC"]:
 					temp.append(vtools.remove_b(self.get_output(["guestproperty", "get", machine_name, f"/VirtualBox/GuestInfo/Net/{i}/{propriety}"]).replace("Value: ", "").replace("\\n", "").replace("\\r", "")))
 
@@ -124,7 +148,7 @@ class vtools:
 		except:
 			pass
 
-		self.log.print("Getted network infos")
+		self.log.print(f"Getted network infos {network}")
 		return network
 
 	def get_attachments(self, machine_name):
@@ -147,4 +171,4 @@ def laucher(other=None):
 	vtools(other)
 		
 if __name__ == "__main__":
-	laucher()
+	laucher('--choco')
