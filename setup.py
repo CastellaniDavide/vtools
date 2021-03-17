2fd0199209a738e721b776501018aaacc77560bf
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-03-17 08:33:36 +0100

    from setuptools import setup import sys,os from requests import get setup(       name='vtools',       version="1.0", # Lastest release       description="vtools",       long_description="Manage vitual machines, getting some informations (eg. OS).",       long_description_content_type="text/markdown",       url="https://github.com/repos/CastellaniDavide/vtools",       author="DavideC03",       author_email="help@castellanidavide.it",       license='GNU',       packages = ['src'],       python_requires=">=3.6",       platforms="linux_distibution",       install_requires=["requests"],       zip_safe=True,       entry_points = {         'console_scripts': [             'vtools=src.vtools:laucher']             },       )

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup
 import sys,os
 from requests import get
-
 setup(
       name='vtools',
       version="1.0", # Lastest release
-      description=get("https://api.github.com/repos/CastellaniDavide/vtools").json()['description'],
-      long_description=get("https://raw.githubusercontent.com/CastellaniDavide/vtools/master/docs/README.md").text,
+      description="vtools",
+      long_description="Manage vitual machines, getting some informations (eg. OS).",
       long_description_content_type="text/markdown",
-      url=get("https://api.github.com/repos/CastellaniDavide/vtools").json()['html_url'],
-      author=get("https://api.github.com/repos/CastellaniDavide/vtools").json()['owner']['login'],
-      author_email=get(f"https://api.github.com/users/{get('https://api.github.com/repos/CastellaniDavide/vtools').json()['owner']['login']}").json()['email'],
+      url="https://github.com/repos/CastellaniDavide/vtools",
+      author="DavideC03",
+      author_email="help@castellanidavide.it",
       license='GNU',
       packages = ['src'],
       python_requires=">=3.6",
       platforms="linux_distibution",
-      install_requires=[i for i in get("https://raw.githubusercontent.com/CastellaniDavide/vtools/master/requirements/requirements.txt").text.split("\n") if not "#" in i and i != ''],
+      install_requires=["requests"],
       zip_safe=True,
       entry_points = {
         'console_scripts': [
