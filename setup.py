5796e430efc31a3512d6a8d15b9e315a026f058e
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-03-17 08:26:30 +0100

    version="1.0"

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from requests import get
 
 setup(
       name='vtools',
-      version=get("https://api.github.com/repos/CastellaniDavide/vtools/tags").json()[0]['name'].replace("v", "") if len(get("https://api.github.com/repos/CastellaniDavide/vtools/tags").json()) > 0 else "1.0", # Lastest release
+      version="1.0", # Lastest release
       description=get("https://api.github.com/repos/CastellaniDavide/vtools").json()['description'],
       long_description=get("https://raw.githubusercontent.com/CastellaniDavide/vtools/master/docs/README.md").text,
       long_description_content_type="text/markdown",
