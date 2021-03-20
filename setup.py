ec7a1ae46d0b1b34ac841ef5652008124c4dfeeb
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-03-20 16:37:47 +0100

    v0.3

--- setup.py
+++ setup.py
@@ -17,29 +17,3 @@ setup(
       install_requires=[i for i in get("https://raw.githubusercontent.com/CastellaniDavide/vtools/master/requirements/requirements.txt").text.split("\n") if not "#" in i and i != ''],
       zip_safe=True
       )
-
-"""
-from setuptools import setup
-import sys,os
-from requests import get
-setup(
-      name='vtoolscd',
-      version="0.1", # Lastest release
-      description="vtools",
-      long_description="Manage vitual machines, getting some informations (eg. OS).",
-      long_description_content_type="text/markdown",
-      url="https://github.com/repos/CastellaniDavide/vtools",
-      author="DavideC03",
-      author_email="help@castellanidavide.it",
-      license='GNU',
-      packages = ['src'],
-      python_requires=">=3.6",
-      platforms="linux_distibution",
-      install_requires=["requests", "tabular-log>=2.5"],
-      zip_safe=True,
-      entry_points = {
-        'console_scripts': [
-            'vtools=src.vtools:laucher']
-            },
-      )
-"""
